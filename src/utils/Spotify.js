0c72baa66b4cbf63ef95ba7e07e2e8bf64c5fff5
Veerakit Chotpititaveepongm <air.veerakit@gmail.com>
2022-03-18 13:32:10 +0700

    minorfixed

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -1,7 +1,7 @@
 // TODO: Get Client ID from https://developer.spotify.com/dashboard/ and put it here
 const clientId = 'f8c1f495050a4fa0990637424b3e92c2';
 
-const redirectUri = 'http://localhost:3000/';
+const redirectUri = 'http://spoti-goo.surge.sh';
 const spotifyUrl = `https://accounts.spotify.com/authorize?response_type=token&scope=playlist-modify-public&client_id=${clientId}&redirect_uri=${redirectUri}`;
 let accessToken = undefined;
 let expiresIn = undefined;
